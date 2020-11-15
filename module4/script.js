5e822c490411c6be81212ea02de50b9d51a6a56f
Jüri Tõnisberg <jyri.tonisberg@gmail.com>
2020-11-15 22:31:15 +0200

    third

--- module4/script.js
+++ module4/script.js
@@ -31,6 +31,9 @@ WARNING!!! WARNING!!!
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
+
+var names = [];
+
 (function(){
   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
@@ -39,6 +42,7 @@ WARNING!!! WARNING!!!
 // using the 'speak' method or either helloSpeaker's or byeSpeaker's
 // 'speak' method.
 // See Lecture 50, part 1
+
 for (name in names) {
 
   // STEP 11:
@@ -48,6 +52,7 @@ for (name in names) {
   // string object's 'toLowerCase' method on the result so we can compare
   // to lower case character 'j' afterwards.
   // Look up these methods on Mozilla Developer Network web site if needed.
+
     var firstLetter = names[name].charAt(0).toLowerCase();
     if (firstLetter==="j") {
       byeSpeaker.speak(names[name]);
@@ -55,10 +60,13 @@ for (name in names) {
     else {
       helloSpeaker.speak(names[name]);
     }
+
   // STEP 12:
   // Compare the 'firstLetter' retrieved in STEP 11 to lower case
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
+
+
 }
 })();
